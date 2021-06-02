73f6370ca71a51598a4bc044ecdfccc3ccee26ee
MILOONI YAGNIK <Milooni21>
2021-06-02 18:41:22 +0530

    Initial commit

---
+++ newsletter.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Welcome <br>
+First Name: <?php echo $_POST["firstname"]; ?><br>
+Last Name: <?php echo $_POST["lastname"]; ?><br>
+Email: <?php echo $_POST["email"]; ?><br>
+Birthdate: <?php echo $_POST["birthdate"]; ?><br>
+
+
+
+</body>
+</html>
